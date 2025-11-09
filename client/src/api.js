baf739170f22ad0b921e00b6d843328b38f46d8d
Llalithsaikumar <l.lalithsaikumar@gmail.com>
2025-11-09 13:13:13 +0530

    Update client API configuration

--- client/src/api.js
+++ client/src/api.js
@@ -1,23 +1,25 @@
 import axios from "axios";
 
+const baseURL =
+  (import.meta?.env?.VITE_API_URL?.replace(/\/+$/, "")) || "http://localhost:4000";
+
 const instance = axios.create({
-	baseURL: import.meta?.env?.VITE_API_URL || "http://localhost:4000/api",
-	withCredentials: false
+  baseURL: `${baseURL}/api`,
+  withCredentials: true, // allow cookies/session auth
 });
 
 function setToken(token) {
-	if (token) {
-		instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-	} else {
-		delete instance.defaults.headers.common["Authorization"];
-	}
+  if (token) {
+    instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete instance.defaults.headers.common["Authorization"];
+  }
 }
 
 export const api = {
-	get: (url, config) => instance.get(url, config),
-	post: (url, data, config) => instance.post(url, data, config),
-	put: (url, data, config) => instance.put(url, data, config),
-	delete: (url, config) => instance.delete(url, config),
-	setToken
+  get: (url, config) => instance.get(url, config),
+  post: (url, data, config) => instance.post(url, data, config),
+  put: (url, data, config) => instance.put(url, data, config),
+  delete: (url, config) => instance.delete(url, config),
+  setToken,
 };
-
